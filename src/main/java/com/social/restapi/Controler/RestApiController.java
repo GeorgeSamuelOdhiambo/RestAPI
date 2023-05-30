e58b99cacf9be29528483ac58dc6f29e9caedc78
Odhiambo George <georgeodhi98@gmail.com>
2023-05-30 10:57:13 +0300

    controller

--- src/main/java/com/social/restapi/Controler/RestApiController.java
+++ src/main/java/com/social/restapi/Controler/RestApiController.java
@@ -2,6 +2,8 @@ package com.social.restapi.Controler;
 
 import com.social.restapi.Services.RestApiServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,6 @@ public class RestApiController {
     @Autowired
     RestApiServices restApiServices;
 
-
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public void home(){}
 }
