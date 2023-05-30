30cf05d48eca47fb124d84ab4e50492e7e200e59
Odhiambo George <georgeodhi98@gmail.com>
2023-05-30 11:46:04 +0300

    random generator

--- src/main/java/com/social/restapi/Controler/RestApiController.java
+++ src/main/java/com/social/restapi/Controler/RestApiController.java
@@ -2,6 +2,8 @@ package com.social.restapi.Controler;
 
 import com.social.restapi.Services.RestApiServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,16 @@ public class RestApiController {
     @Autowired
     RestApiServices restApiServices;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public void home(){}
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public String home(){
+        return "Home";
+    }
+
+    @RequestMapping(value = "/otp", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public String otpValidation(){
+        return restApiServices.SendString();
+    }
+
+    @RequestMapping(value = "/rr", method = RequestMethod.GET)
+    public void otpUserValidation(){}
 }
--- src/main/java/com/social/restapi/Services/RestApiServices.java
+++ src/main/java/com/social/restapi/Services/RestApiServices.java
@@ -2,13 +2,17 @@ package com.social.restapi.Services;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
 public class RestApiServices {
     public void SendText(){
 
     }
 
-    public String SendString(String Name){
-        return  Name;
+    public String SendString(){
+        Random random = new Random();
+        int otp = random.nextInt(900000) + 100000;
+        return String.valueOf(otp);
     }
 }
